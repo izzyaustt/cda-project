4d4bf244acdf1767b2bdabb46411909292cda11b
jasonmahant <jasonmahantsunny@gmail.com>
2025-04-07 20:39:34 -0400

    Update project.c

--- project.c
+++ project.c
@@ -311,7 +311,7 @@ int instruction_decode(unsigned op,struct_controls *controls)
 /* 5 Points */
 void read_register(unsigned r1,unsigned r2,unsigned *Reg,unsigned *data1,unsigned *data2)
 {
-//test
+//test 777
 }
 
 
